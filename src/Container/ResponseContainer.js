858da6b3137885acf6aa592d0327a3fc0c0d307f
TuanDao2002 <s3877347@rmit.edu.vn>
2022-01-22 22:09:11 +0700

    handle response from api

--- src/Container/ResponseContainer.js
+++ src/Container/ResponseContainer.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ResponseField } from '../Presentational/ResponseField';
 
 export function ResponseContainer(props) {
-    if (props.endpoint === '') return <ResponseField/>;
+    const [response, setResponse] = useState("");
 
-    // let isLoading = false;
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+    useEffect(() => {
+        setResponse("Waiting");
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
 
-    xhr.open('GET', props.endpoint);
-    xhr.send();
+        xhr.open('GET', props.endpoint);
+        xhr.send();
 
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            // let response = xhr.response;
-            // console.log(response);
-            return <ResponseField response={JSON.stringify(xhr.response)}/>
-        } 
-        // else {
-        //     isLoading = true;
-        //     return <ResponseField isLoading={isLoading}/>
-        // }
-    };
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                var status = xhr.status;
+                if (status === 0 || (status >= 200 && status < 400)) {
+                    setResponse(xhr.response);
+                } else if (status === 404) {
+                    setResponse("Not found");
+                }
+            } 
+        };
+    }, [props.endpoint])
 
-    return <ResponseField />;
+    
+    return <ResponseField response={response}/>
 }
--- src/Presentational/ResponseField.js
+++ src/Presentational/ResponseField.js
@@ -1,14 +1,52 @@
 import React from "react";
 
 export function ResponseField(props) {
-    if (props.response) {
-        console.log(props.response);
+    const handleResponse = () => {
+        const response = props.response;
+
+        if (!response) {
+            return <p id='warning'>You have not typed a word</p>;
+        }
+
+        if (response === "Waiting") {
+            return <h3 style= {{textAlign: "center"}}>Loading...</h3>;
+        }
+
+        if (response === "Not found") {
+            return(
+                <>
+                    <p id='warning'>Try again!</p>
+                    <p id='warning'>There is no definition for this word</p>
+                </>
+            ) 
+        }
+
+        let wordDefinitions = [];
+        response.forEach((dictionaryObject, index) => {
+            const len = dictionaryObject.meanings.length;
+            
+            for (let i = 0; i < len; i++){
+                const partOfSpeech = dictionaryObject.meanings[i].partOfSpeech;
+                wordDefinitions.push(<p id="partOfSpeech" key={`${index} ${i}`}>+ {partOfSpeech}:</p>);
+
+                const definitions = dictionaryObject.meanings[i].definitions;
+                let definitionsArray = []
+                definitions.forEach((element, index) => {
+                    definitionsArray.push(<p id="element" key={`child ${index}`}>{`--> ${element.definition}`}</p>);
+                });
+                
+                wordDefinitions.push(definitionsArray);
+            }
+        })
+            
+        return <div id="display">{wordDefinitions}</div>
+
     }
+
     return (
         <div id="responseField">
-            <p id="def">Definition {props.response}</p>
-            {/* {props.isLoading && <h3 style='text-align: center'>Loading...</h3>} */}
-            {/* {props.response && console.log(props.response)} */}
+            <p id="def">Definition </p>
+            {handleResponse()}
         </div>
     )
 }
